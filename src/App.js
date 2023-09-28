7b16bef803b3fbfbf369fdee03e4fd20ac30c5db
365saiom <saiom365@naver.com>
2023-09-28 18:34:28 +0900

    edit opening pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { HashRouter, Route } from "react-router-dom";
+import { HashRouter, Routes, Route, BrowserRouter } from "react-router-dom";
 import Home from "./routes/Home";
 import About from "./routes/About";
 // import axios from "axios";
@@ -115,10 +115,14 @@ import About from "./routes/About";
 
 function App() {
   return (
-    <HashRouter>
-      <Route path="/" component={Home} />
-      <Route path="/about" component={About} />
-    </HashRouter>
+    <BrowserRouter>
+      <HashRouter>
+        <Routes>
+          <Route path="/" exact={true} element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </HashRouter>
+    </BrowserRouter>
   );
 }
 
